65410534f8f40445c20064ef511cac1c9edd413a
Alex-VSH <aisb@tut.by>
2023-06-29 16:12:57 +0300

    add new func and dockstrings

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def capital_letters(str):
+    """Функция делает заглавными буквы подаваемой на ввод строки"""
     return str.upper()
+
+def title_string(str):
+    """Функция делает заглавными первые буквы слов в строке"""
+    return str.title()
 
